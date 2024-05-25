f3b2a0b711de74609beedc5f9838c6b0d145a753
vvaugusto <victorhyuuk1@gmail.com>
2024-05-25 01:47:50 -0300

    feature/merge-all: artist resource tests

--- src/test/java/br/com/vvaug/spotifyutils/resource/ArtistsResourceTest.java
+++ src/test/java/br/com/vvaug/spotifyutils/resource/ArtistsResourceTest.java
@@ -1,13 +1,17 @@
 package br.com.vvaug.spotifyutils.resource;
 
+import br.com.vvaug.spotifyutils.response.ArtistAlbumResponse;
 import br.com.vvaug.spotifyutils.response.ArtistResponse;
+import br.com.vvaug.spotifyutils.response.ArtistTopTracksResponse;
+import br.com.vvaug.spotifyutils.response.SeveralArtistsResponse;
+import br.com.vvaug.spotifyutils.usecase.GetArtistAlbumUseCase;
+import br.com.vvaug.spotifyutils.usecase.GetArtistTopTracksUseCase;
 import br.com.vvaug.spotifyutils.usecase.GetArtistUseCase;
+import br.com.vvaug.spotifyutils.usecase.GetSeveralArtistsUseCase;
 import br.com.vvaug.spotifyutils.utils.TestUtils;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
@@ -23,17 +27,22 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ArtistsResourceTest {
-
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private GetArtistUseCase getArtistUseCase;
+    @MockBean
+    private GetSeveralArtistsUseCase getSeveralArtistsUseCase;
 
+    @MockBean
+    private GetArtistTopTracksUseCase getArtistTopTracksUseCase;
 
+    @MockBean
+    private GetArtistAlbumUseCase getArtistAlbumUseCase;
     @Test
-    public void getArtist() throws Exception {
+    public void getArtistTest() throws Exception {
 
+        //TODO dar valores ao objeto
         ArtistResponse response = ArtistResponse.builder().build();
 
         when(getArtistUseCase.execute(anyString(), anyString())).thenReturn(response);
@@ -42,5 +51,52 @@ public class ArtistsResourceTest {
                         .header(HttpHeaders.AUTHORIZATION, TestUtils.AUTHORIZATION)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is(200));
+
+    }
+
+    @Test
+    public void getArtistsTest() throws Exception {
+
+        //TODO
+        SeveralArtistsResponse severalArtistsResponse = SeveralArtistsResponse.builder().build();
+        when(getSeveralArtistsUseCase.execute(anyString(), anyString())).thenReturn(severalArtistsResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/artists")
+                        .queryParam("ids", "49Wb8WyRUtoyKKO01HkRzI")
+                        .header(HttpHeaders.AUTHORIZATION, TestUtils.AUTHORIZATION)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+
+
+    }
+
+    @Test
+    public void getAlbumsTest() throws Exception {
+
+        //TODO
+        ArtistAlbumResponse albumResponse = ArtistAlbumResponse.builder().build();
+        when(getArtistAlbumUseCase.execute(anyString(), anyString())).thenReturn(albumResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/artists/49Wb8WyRUtoyKKO01HkRzI/albums")
+                        .header(HttpHeaders.AUTHORIZATION, TestUtils.AUTHORIZATION)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+
+
+    }
+
+    @Test
+    public void getTopTracksTest() throws Exception {
+
+        //TODO
+        ArtistTopTracksResponse artistTopTracksResponse = ArtistTopTracksResponse.builder().build();
+        when(getArtistTopTracksUseCase.execute(anyString(), anyString())).thenReturn(artistTopTracksResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/artists/49Wb8WyRUtoyKKO01HkRzI/top-tracks")
+                        .header(HttpHeaders.AUTHORIZATION, TestUtils.AUTHORIZATION)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+
+
     }
 }
