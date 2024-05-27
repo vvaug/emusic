a42de830a6178d6062802326042eb4d6b48b2c73
vvaugusto <victorhyuuk1@gmail.com>
2024-05-27 20:02:52 -0300

    emusicuserds: novo projeto - microservico de dados de usuarios

---
+++ emusicuserds/src/test/java/br/com/vvaug/EmusicuserdsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vvaug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmusicuserdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
