5733e2d4c406b80533188882a57aed990698fefb
vvaugusto <victorhyuuk1@gmail.com>
2024-05-29 18:18:49 -0300

    microserviço gateway

---
+++ emusicgateway/src/test/java/br/com/vvaug/emusicgateway/EmusicgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vvaug.emusicgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmusicgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
