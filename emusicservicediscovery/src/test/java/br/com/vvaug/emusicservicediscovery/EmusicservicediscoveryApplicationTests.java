9bda3491d7af8683fb16a1e027fe6668c19b47cf
vvaugusto <victorhyuuk1@gmail.com>
2024-05-29 18:16:59 -0300

    novo microserviço para service discovery

---
+++ emusicservicediscovery/src/test/java/br/com/vvaug/emusicservicediscovery/EmusicservicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vvaug.emusicservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmusicservicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
