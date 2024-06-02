0d5929b4669f77cb2be4f20e228c2403a92e828e
vvaugusto <victorhyuuk1@gmail.com>
2024-06-02 18:04:04 -0300

    feature/service-discovery-impl: implementando service-discovery

--- emusicauth/src/main/java/br/com/vvaug/auth/client/EmusicUserDsClient.java
+++ emusicauth/src/main/java/br/com/vvaug/auth/client/EmusicUserDsClient.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "e-music-user-ds-client", url = "${feign.clients.emusicuserds}")
+@FeignClient("emusicuserds")
 public interface EmusicUserDsClient {
 
-    @GetMapping("/user/{username}")
+    @GetMapping("/emusicuserds/v1/user/{username}")
     UserResponse getUser(@PathVariable("username") String username);
 
 
